1dee2e3b17be266cfcb73ce4a3690c3cfdb5758e
gacmalony <gocmenmertkaan@gmail.com>
2024-01-24 17:49:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/navapp/HomeFragment.kt
@@ -0,0 +1,42 @@
+package com.example.navapp
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.navapp.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding:FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home, container,false)
+
+        binding.button.setOnClickListener(){
+
+
+            if(!TextUtils.isEmpty(binding.editTextText.text.toString())){
+                val bundle = bundleOf("first" to binding.editTextText.text.toString())
+
+                it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment,bundle)
+            }else{
+                Toast.makeText(activity, "Enter something please!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        return binding.root
+    }
+
+}
---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+import com.android.build.gradle.internal.dsl.decorator.SupportedPropertyType.Collection.List.type
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.7.6"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
+
+
+
+
+
+
